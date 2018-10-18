9d28d0ff85a141dcbcc10672acd89b50f65bc211
himanshu <CSE505.1180himanshu@gmail.com>
2018-10-18 16:18:07 +0530

    New fixes

--- app.js
+++ app.js
@@ -4,6 +4,7 @@
 angular.module('DIApp', [])
 .controller('DIController', DIController);
 
+DIController.$inject = ['$scope', '$filter'];
 function DIController($scope, $filter) {
   $scope.name = 'Himanshu';
 
